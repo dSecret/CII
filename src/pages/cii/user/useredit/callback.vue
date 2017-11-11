a3dcb8e2f7a4a5fdc498d93b2ff0bc7e1a9e9824
dSecret <ajrathore786@gmail.com>
2017-11-11 14:52:15 +0530

    Revert "Revert "nxt""
    This reverts commit efabe79250ebe7fc36cd21ade1edd87874762c17.

---
+++ src/pages/cii/user/useredit/callback.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="padding-bottom:3vh;">
+      <div style="margin-bottom:60px;">
+          Logging you in!!
+      </div>
+  </div>
+</template>
+
+<script>
+import { setIdToken, setAccessToken} from '../../../../../utils/auth';
+import auth0 from 'auth0-js'
+
+export default {
+  data () {
+    return {
+    }
+  },
+    mounted() {
+    this.$nextTick(() => {
+      setAccessToken();
+      setIdToken();
+      window.location.href='/'
+    });
+  },
+}
+</script>
+
+<style>
+
+</style>
